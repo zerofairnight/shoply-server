ae9fe8092c44e550f0f8d0c60768e9fb17b15805
zerofairnight <zerofairnight@gmail.com>
2019-01-30 19:54:12 +0100

    add app middleware group

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -37,6 +37,10 @@ class Kernel extends HttpKernel
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
         ],
 
+        'app' => [
+            \Illuminate\Routing\Middleware\SubstituteBindings::class,
+        ],
+
         'api' => [
             'throttle:60,1',
             'bindings',
