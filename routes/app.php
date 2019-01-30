bb9478e30352e8a017ef81a0eb50e07ea3da1fd9
zerofairnight <zerofairnight@gmail.com>
2019-01-30 20:12:09 +0100

    route name prefix app.

--- routes/app.php
+++ routes/app.php
@@ -5,3 +5,7 @@ use Illuminate\Http\Request;
 Route::middleware('auth:app')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::name('app.')->group(function () {
+    Route::apiResource('shops', 'ShopController');
+});
