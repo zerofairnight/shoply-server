5ab49642114c95fd325bd9f0a9ccc11e85522568
Matteo <matteobonanno1990@gmail.com>
2019-01-30 20:49:35 +0100

    Add lastname and gender in user migration and factory

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -16,6 +16,8 @@ use Faker\Generator as Faker;
 $factory->define(App\Models\User::class, function (Faker $faker) {
     return [
         'name' => $faker->name,
+        'lastname' => $faker->surname,
+        'gender' => $faker->title('Male'|'Female'),
         'email' => $faker->unique()->safeEmail,
         'email_verified_at' => now(),
         'password' => '$2y$10$TKh8H1.PfQx37YgCzwiKb.KjNyWgaHb9cbcoQgdIVFlYg7B77UdFm', // secret
