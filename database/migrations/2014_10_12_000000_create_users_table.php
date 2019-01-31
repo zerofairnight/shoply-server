563e75c3fa1a8ced11f9d59477b529b25bab1967
zerofairnight <zerofairnight@gmail.com>
2019-01-31 10:47:11 +0100

    adopt iso 5218 and drop last_name

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,8 +16,7 @@ class CreateUsersTable extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
             $table->string('name');
-            $table->string('last_name');
-            $table->string('gender');
+            $table->tinyInteger('sex'); // ISO 5218
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
