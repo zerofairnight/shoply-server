149ff84f565677ebb31928c2dab9b32becf4e24b
zerofairnight <zerofairnight@gmail.com>
2019-01-31 11:40:55 +0100

    update local database to be more streamlined

--- database/seeds/LocalDatabaseSeeder.php
+++ database/seeds/LocalDatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 use App\Models\User;
 use App\Models\Shop;
+use App\Models\Article;
 use Illuminate\Database\Seeder;
 
 class LocalDatabaseSeeder extends Seeder
@@ -17,7 +18,12 @@ class LocalDatabaseSeeder extends Seeder
         $user = factory(User::class)->create([
             'name' => 'Test User',
             'email' => 'a@a.com'
-        ])->shops()->attach(factory(Shop::class)->create()); // note that the shop can have a different owner
+        ]);
+
+        $shop = factory(Shop::class)->create(['creator_id' => $user->id]);
+        $shop->articles()->saveMany(factory(Article::class, 50)->create(['shop_id' => $shop->id]));
+
+        $user->shops()->attach($shop); // note that the shop can have a different owner
 
         factory(User::class, 50)->create();
     }
